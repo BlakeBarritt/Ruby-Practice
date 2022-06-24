91b68e0191514dc7b0a6324204db0ad48126863c
Blake <77072507+BlakeBarritt@users.noreply.github.com>
2022-06-24 12:38:21 -0700

    Add files via upload

---
+++ Assign_6.rb
@@ -0,0 +1,24 @@
+class Student
+  attr_accessor :first_name, :last_name, :email, :username, :password
+ 
+  def initialize(firstname, lastname, username, email, password)
+    @first_name = firstname
+    @last_name = lastname
+    @username = username
+    @email = email
+    @password = password
+  end
+ 
+  def to_s
+    "First name: #{@first_name}, Last name: #{@last_name}, Username: #{@username}, email address: #{@email}"
+  end
+  
+end
+ 
+blake = Student.new("Blake", "Barritt", "barritt1", "blake@example.com", "password1")
+john = Student.new("John", "Doe", "john1", "john1@example.com","password2")
+puts blake
+puts john
+blake.last_name = john.last_name
+puts "Blake is altered"
+puts blake
